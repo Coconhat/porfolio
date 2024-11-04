50b16b15669e5c3dc35f1eea138f18a4734dc762
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-11-04 21:47:26 +0800

    added project section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -5,20 +5,21 @@ import grainImage from "@/assets/images/grain.jpg";
 import StarIcon from "@/assets/icons/star.svg";
 import { HeroOrbit } from "@/components/HeroOrbit";
 import SparkleIcon from "@/assets/icons/sparkle.svg";
+
 export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom, transparent,black_10%,black_70%,transparent)]">
+      <div className="absolute inset-0 [mask-image:linear-gradient(to_bottom, transparent, black_10%, black_70%, transparent)] z-10">
         <div
           className="absolute inset-0 -z-30 opacity-5"
           style={{
             backgroundImage: `url(${grainImage.src})`,
           }}
         ></div>
-        <div className=" size-[620px] hero-ring"></div>
-        <div className=" size-[820px] hero-ring"></div>
-        <div className=" size-[1020px] hero-ring"></div>
-        <div className=" size-[1220px] hero-ring"></div>
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
         <HeroOrbit size={800} rotation={-72}>
           <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
@@ -51,12 +52,12 @@ export const HeroSection = () => {
         </HeroOrbit>
       </div>
 
-      <div className="container">
+      <div className="container relative z-20">
         <div className="flex flex-col items-center">
           <Image
             src={memojiImage}
             className="size-[100px]"
-            alt="Personpeeking from behind laptop"
+            alt="Person peeking from behind laptop"
           />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
             <div className="bg-green-500 size-2.5 rounded-full"></div>
@@ -67,7 +68,7 @@ export const HeroSection = () => {
         </div>
         <div className="max-w-lg mx-auto">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
-            Building Exceptional user Experiences
+            Building Exceptional User Experiences
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
@@ -76,7 +77,7 @@ export const HeroSection = () => {
             eaque non totam atque.
           </p>
         </div>
-        <div className="flex flex-col md:flex-row  justify-center items-center mt-8 gap-4 ">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4 z-30">
           <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
@@ -87,6 +88,30 @@ export const HeroSection = () => {
           </button>
         </div>
       </div>
+    </div>
+  );
+};
+
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center fixed top-3 w-full z-10">
+      <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Projects
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a
+          href="#"
+          className="nav-item bg-white text-grey-900 hover:bg-white/70 text-gray-900"
+        >
+          Contact
+        </a>
+      </nav>
     </div>
   );
 };
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,46 +1,128 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
-
+import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import apodnasa from "@/assets/images/apodnasa.png";
+import currencyconverter from "@/assets/images/currencyconverter2.png";
 const portfolioProjects = [
   {
-    company: "Acme Corp",
-    year: "2022",
-    title: "Dark Saas Landing Page",
+    company: "",
+    year: "2024",
+    title: "NASA APOD NEWS",
     results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+      {
+        title:
+          " Provides daily updates on NASA's Astronomy Picture of the Day (APOD), showcasing space images and insights.",
+      },
+      {
+        title:
+          "Users can click on any news item to open a modal, which provides a full description, image, and details.",
+      },
+      {
+        title:
+          "Optimized for fast loading, with a focus on mobile compatibility",
+      },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
-    image: darkSaasLandingPage,
+    link: "https://coconhat.github.io/NASA-APOD-News/",
+    image: apodnasa,
   },
   {
-    company: "Innovative Co",
-    year: "2021",
-    title: "Light Saas Landing Page",
+    company: "",
+    year: "2024",
+    title: "Currency Converter",
     results: [
-      { title: "Boosted sales by 20%" },
-      { title: "Expanded customer reach by 35%" },
-      { title: "Increased brand awareness by 15%" },
+      {
+        title:
+          "Provides fast, reliable currency conversions for a wide range of international currencies.",
+      },
+      {
+        title:
+          "implifies currency conversion, particularly beneficial for travelers, businesses, and online shoppers.",
+      },
+      {
+        title:
+          ": Uses a real-time currency exchange API to provide up-to-date conversion rates.",
+      },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
-    image: lightSaasLandingPage,
+    link: "https://coconhat.github.io/Currency-converter/",
+    image: currencyconverter,
   },
-  {
-    company: "Quantum Dynamics",
-    year: "2023",
-    title: "AI Startup Landing Page",
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
-    ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
-    image: aiStartupLandingPage,
-  },
+  
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16 lg:py-24 ">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">
+            Portfolio Highlights
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Feature Projects
+        </h2>
+        <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">
+          Each project represents a unique vision transformed into an engaging
+          digital reality
+        </p>
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none  "
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5 "
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex font-bold uppercase tracking-widest text-sm gap-2 text-transparent bg-clip-text">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="font-serif text-2xl mt-2 md:text-4xl md:mt-5">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5 " />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm md:text-base text-white/50 "
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 md:w-auto px-6">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt:0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
