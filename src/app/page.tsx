533b5ab76aa00d926c40c33e9685b3943dadbac8
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-12-21 02:16:57 +0800

    fix:deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
-import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
