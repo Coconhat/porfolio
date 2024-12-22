b9931048a0133ef8b135ffbbfbd165c9a7c0451b
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-12-22 15:14:34 +0800

    fix: refactor About and Contact sections; remove unnecessary styles and enhance button component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -183,11 +183,9 @@ export const AboutSection = () => {
                   src={mapImage}
                   alt="map"
                   className="h-full w-full object-cover object-left-top"
-                  style={{ filter: "grayscale(100%)" }}
+                  
                 />
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full  after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-vercel-gray-700">
-                  <div className="absolute inset-0 rounded-full bg-vercel-gray-800 -z-20 animate-ping [animation-duration:2s]"></div>
-                  <div className="absolute inset-0 rounded-full bg-vercel-gray-800 -z-10"></div>
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full">
                   <Image src={smileMemoji} alt="memoji" className="size-20" />
                 </div>
               </Card>
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
+import { RainbowButton } from "@/components/ui/rainbow-button";
 
 export const ContactSection = () => {
   return (
     <section className="bg-background">
       <div className="py-16 pt-12 lg:py-24 lg:pt-20 bg-vercel-white text-vercel-black">
         <div className="container mx-auto">
-          <div className="bg-gray-900 text-white py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+          <div className="bg-gray-900 text-white py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-10">
             <div
               className="absolute inset-0 opacity-5 -z-10"
               style={{
@@ -17,19 +18,17 @@ export const ContactSection = () => {
             ></div>
             <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
               <div>
-                <h2 className="text-2xl md:text-3xl">
-                  Like what you see?
-                </h2>
+                <h2 className="text-2xl md:text-3xl">Like what you see?</h2>
                 <p className="text-sm mt-2 md:text-base">
                   Let&apos;s connect and discuss how I can help you achieve your
                   goals.
                 </p>
               </div>
-              <div className="lg:ml-20">
-                <button className="text-white bg-gray-800 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-700">
-                  <span className="font-semibold">Get in touch</span>
+              <div className="lg:ml-20 z-20">
+                <RainbowButton className="text-white bg-vercel-black inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-700">
+                  <span className="font-semibold cursor-pointer">Get in touch</span>
                   <ArrowUpRightIcon className="w-4 h-4" />
-                </button>
+                </RainbowButton>
               </div>
             </div>
           </div>
